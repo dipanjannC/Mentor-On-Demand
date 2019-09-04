58b892709748c6a840bfa1d8aa2a5868f36d02ab
Chowdhury <761334@cognizant.com>
2019-09-04 15:42:10 +0530

    Bootstrap Updated

---
+++ backend/src/main/java/com/mentor/web/controller/AuthenticationController.java
@@ -0,0 +1,43 @@
+package com.mentor.web.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.web.bind.annotation.*;
+
+import com.mentor.web.model.User;
+import com.mentor.web.security.config.JwtTokenUtil;
+import com.mentor.web.security.model.ApiResponse;
+import com.mentor.web.security.model.AuthToken;
+import com.mentor.web.security.model.LoginUser;
+import com.mentor.web.service.UserService;
+
+
+
+@CrossOrigin(origins = "*", maxAge = 3600)
+@RestController
+@RequestMapping("/token")
+public class AuthenticationController {
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private JwtTokenUtil jwtTokenUtil;
+
+    @Autowired
+    private UserService userService;
+
+    @RequestMapping(value = "/generate-token", method = RequestMethod.POST)
+    public ApiResponse<AuthToken> register(@RequestBody LoginUser loginUser) throws AuthenticationException {
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(loginUser.getEmail(), loginUser.getPassword()));
+        final User user = userService.findUser(loginUser.getEmail());
+        final String token = jwtTokenUtil.generateToken(user);
+     
+        return new ApiResponse<>(200, "success",new AuthToken(token, user.getEmail(),user.getUsertype(),user.getUserId()));
+    }
+
+}
--- backend/src/main/java/com/mentor/web/security/model/AuthToken.java
+++ backend/src/main/java/com/mentor/web/security/model/AuthToken.java
@@ -4,15 +4,23 @@ public class AuthToken {
 
 	private String token;
 	private String username;
+	private String usertype;
+	private long userId;
 
 	public AuthToken() {
 
 	}
 
-	public AuthToken(String token, String username) {
+
+	public AuthToken(String token, String username, String usertype, long userId) {
+		super();
 		this.token = token;
 		this.username = username;
+		this.usertype = usertype;
+		this.userId = userId;
 	}
+
+
 
 	public AuthToken(String token) {
 		this.token = token;
@@ -33,4 +41,20 @@ public class AuthToken {
 	public void setUsername(String username) {
 		this.username = username;
 	}
+	public String getUsertype() {
+		return usertype;
+	}
+
+	public void setUsertype(String usertype) {
+		this.usertype = usertype;
+	}
+
+	public long getUserId() {
+		return userId;
+	}
+
+	public void setUserId(long userId) {
+		this.userId = userId;
+	}
+
 }
